9f8784d3def115f4c848350840fd8e20e198c35e
pirpyn <payen.pierre@gmail.com>
2023-10-25 08:50:14 +0000

    Check server status every minute in Bot activity

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,22 @@
 const synchronizeSlashCommands = require('../modules/sync_commands')
 const {ActivityType} = require('discord.js')
 const config = require('../config.json');
+const gamedig = require('../modules/gamedig')
+
+function setPresence(client){
+	gamedig.GetStatus().then((server_status) => {
+		if (server_status.online)
+			client.user.setActivity(`${server_status.map}: ${server_status.players} players`, {type: ActivityType.Custom});
+		else
+			client.user.setActivity(`${server_status.host} offline`, {type: ActivityType.Custom});
+	});
+}
 
 module.exports = {
 	name: 'ready',
 	async execute(client) {
-		console.log(`Connecté en tant que ${client.user.username}`)
-		client.user.setActivity(`/bord`, {type: ActivityType.Watching})
+		console.log(`Connecté en tant que ${client.user.username}`);
+		setInterval(setPresence,60*1000,client);
 		await synchronizeSlashCommands(client,
 			client.commands.map((c) => c.data),
 			{
